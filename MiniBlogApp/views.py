784f3d4ee31fb92fa6238de0467e79b79ec29276
Rafael Rosa <rafael.ricardo@outlook.com>
2022-06-27 22:00:13 -0500

    Initial commit

---
+++ MiniBlogApp/views.py
@@ -0,0 +1,17 @@
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from .models import *
+from .forms import *
+from django.db.models import Q
+
+# Create your views here.
+
+def inicio(request):
+
+    return render(request, "MiniBlogApp/index.html", {})
+
+def base(request):
+    
+    return render(request, "MiniBlogApp/base.html", {})
---
+++ README.md
@@ -0,0 +1,2 @@
+# MiniBlogCoder
+ proyectofinal
